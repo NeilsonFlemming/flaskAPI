ab914ef55071d9987423cadcbb64b0992ae46584
Neilson Flemming <NeilsonFlemming@gmail.com>
2020-02-19 11:16:59 -0500

    routing setup - creating graphql endpoint

---
+++ api/restful_api.py
@@ -0,0 +1,62 @@
+from flask import Blueprint, jsonify, request
+import requests
+
+restful_api = Blueprint('restful_api', __name__)
+
+@restful_api.errorhandler(404)
+def page_not_found(e):
+    return jsonify(Error='The resource (endpoint) could not be found'), 404
+
+@restful_api.route('/')
+def home():
+    return jsonify(Message='Welcome to Neilson\'s flask resultful weather API'), 200
+
+@restful_api.route('/weather/current')
+def current_weather():
+    # Check if an zip was provided as part of the URL.
+    # If zip is provided, assign it to a variable.
+    # If no ID is provided, display an error via api.
+    if 'zip' in request.args:
+        # Try to assign ZIP to integer for lookup purposes
+        try:
+            if len(request.args['zip']) != 5:
+                raise ValueError('Zip code length is less than or greater than 5')
+            else:
+                zip_code = int(request.args['zip'])
+        except:
+            # Throw error if not valid Zip Code
+            return jsonify(Error='Zip provided is not a valid zip code')
+
+    else:
+        return jsonify(
+            Error='No zip code query provided. Please provide an ID to lookup by - example.com/api/weather/current?zip=10018')
+
+    r = requests.get(
+        'http://api.openweathermap.org/data/2.5/weather?zip={}&APPID=929be9dbced9ad5fe63ae8c31134eb3f'.format(zip_code))
+
+    return r.json(), 200
+
+@restful_api.route('/weather/forecast')
+def forcast_weather():
+    # Check if an zip was provided as part of the URL.
+    # If zip is provided, assign it to a variable.
+    # If no ID is provided, display an error via api.
+    if 'zip' in request.args:
+        # Try to assign ZIP to integer for lookup purposes
+        try:
+            if len(request.args['zip']) != 5:
+                raise ValueError('Zip code length is less than or greater than 5')
+            else:
+                zip_code = int(request.args['zip'])
+        except:
+            # Throw error if not valid Zip Code
+            return jsonify(Error='Zip provided is not a valid zip code')
+
+    else:
+        return jsonify(
+            Error='No zip code query provided. Please provide an ID to lookup by - example.com/api/weather/forecast?zip=10018')
+
+    r = requests.get(
+        'http://api.openweathermap.org/data/2.5/forecast?zip={}&APPID=929be9dbced9ad5fe63ae8c31134eb3f'.format(zip_code))
+
+    return r.json(), 200
--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask
-from api.api import api
+from flask import Flask, jsonify
+from api.restful_api import restful_api
 
 #https://programminghistorian.org/en/lessons/creating-apis-with-python-and-flask
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
-app.register_blueprint(api, url_prefix='/api')
+app.register_blueprint(restful_api, url_prefix='/api')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return jsonify(Error='The resource could not be found'), 404
+
+@app.route('/', methods=['GET'])
+def home():
+    return jsonify(Message='Welcome to Neilson\'s flask app'), 200
 
 if __name__=='__main__':
     app.run()
