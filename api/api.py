abcbd41ac432be5d451df84ac681f97348250d6d
Neilson Flemming <NeilsonFlemming@gmail.com>
2020-02-18 21:19:32 -0500

    Updating API to weather

--- api/api.py
+++ api/api.py
@@ -1,57 +1,33 @@
 from flask import Flask
 from flask import jsonify, request
+import requests as Requests
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-books = [
-    {'id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge',
-     'first_sentence': 'The coldsleep itself was dreamless.',
-     'year_published': '1992'},
-    {'id': 1,
-     'title': 'The Ones Who Walk Away From Omelas',
-     'author': 'Ursula K. Le Guin',
-     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-     'published': '1973'},
-    {'id': 2,
-     'title': 'Dhalgren',
-     'author': 'Samuel R. Delany',
-     'first_sentence': 'to wound the autumnal city.',
-     'published': '1975'}
-]
-
-
 @app.route('/', methods=['GET'])
 def home():
-    return '''<h1>Distant Reading Archive</h1>
-<p>A prototype API for distant reading of science fiction novels.</p>'''
+    return '''<h1>Neilson's python flask weather API </h1>
+    <p>A prototype API for distant reading of science fiction novels.</p>'''
 
 @app.errorhandler(404)
 def page_not_found(e):
     return "<h1>404</h1><p>The resource could not be found.</p>", 404
 
-
-# A route to return all of the available entries in our catalog.
-@app.route('/api/books/all', methods=['GET'])
-def api_all():
-    return jsonify(books)
-
-@app.route('/api/books', methods=['GET'])
-def book_id():
-    # Check if an ID was provided as part of the URL.
-    # If ID is provided, assign it to a variable.
-    # If no ID is provided, display an error in the browser.
-    if 'id' in request.args:
-        # Try to assign ID to integer for lookup purposes
+@app.route('/api/weather/current', methods=['GET'])
+def current_weather():
+    # Check if an zip was provided as part of the URL.
+    # If zip is provided, assign it to a variable.
+    # If no ID is provided, display an error via api.
+    if 'zip' in request.args:
+        # Try to assign ZIP to integer for lookup purposes
         try:
             id = int(request.args['id'])
         except:
-        # Throw error if not valid integer
-            return jsonify(Error='ID provided is not a valid integer')
+            # Throw error if not valid Zip Code
+            return jsonify(Error='ID provided is not a valid zip code')
     else:
-        return jsonify(Error='No ID query field provided. Please provide an ID to lookup by - example.com/api/?id=1')
+        return jsonify(Error='No zip query field provided. Please provide an ID to lookup by - example.com/api/weather/current?zip=10018')
 
     # Create an empty list for our results
     results = []
