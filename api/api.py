8804012b2173dad91926fd05b27632ca861bf874
Neilson Flemming <NeilsonFlemming@gmail.com>
2020-02-18 21:58:59 -0500

    Added current weather api endpoint with validation

--- api/api.py
+++ api/api.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import jsonify, request
-import requests as Requests
+import requests
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -8,39 +8,35 @@ app.config["DEBUG"] = True
 @app.route('/', methods=['GET'])
 def home():
     return '''<h1>Neilson's python flask weather API </h1>
-    <p>A prototype API for distant reading of science fiction novels.</p>'''
+    <p>A prototype API for weather data.</p>'''
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return "<h1>404</h1><p>The resource could not be found.</p>", 404
+    return jsonify(Error='The resource could not be found'), 404
 
 @app.route('/api/weather/current', methods=['GET'])
 def current_weather():
     # Check if an zip was provided as part of the URL.
     # If zip is provided, assign it to a variable.
     # If no ID is provided, display an error via api.
-    if 'zip' in request.args:
+    if 'zip' in request.args :
         # Try to assign ZIP to integer for lookup purposes
         try:
-            id = int(request.args['id'])
+            if len(request.args['zip']) !=5:
+                raise ValueError('Zip code length is less than or greater than 5')
+            else:
+                zip_code = int(request.args['zip'])
         except:
             # Throw error if not valid Zip Code
-            return jsonify(Error='ID provided is not a valid zip code')
+            return jsonify(Error='Zip provided is not a valid zip code')
+
     else:
-        return jsonify(Error='No zip query field provided. Please provide an ID to lookup by - example.com/api/weather/current?zip=10018')
+        return jsonify(Error='No zip code query provided. Please provide an ID to lookup by - example.com/api/weather/current?zip=10018')
 
-    # Create an empty list for our results
-    results = []
+    r = requests.get(
+        'http://api.openweathermap.org/data/2.5/weather?zip={}&APPID=929be9dbced9ad5fe63ae8c31134eb3f'.format(zip_code))
 
-    # Loop through the data and match results that fit the requested ID.
-    # IDs are unique, but other fields might return many results
-    for book in books:
-        if book['id'] == id:
-            results.append(book)
-
-    # Use the jsonify function from Flask to convert our list of
-    # Python dictionaries to the JSON format.
-    return jsonify(results)
+    return r.json(),200
 
 
 if __name__ == '__main__':
