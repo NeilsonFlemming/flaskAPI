cddb31c043023d89c9520a62112dc420aed2314e
Neilson Flemming <NeilsonFlemming@gmail.com>
2020-04-04 18:03:58 -0400

    updated endpoint JSON responces

--- api/restful_api.py
+++ api/restful_api.py
@@ -45,18 +45,19 @@ def currentWeather():
         'temp': weatherData['main']['temp'],
         'temp_max': weatherData['main']['temp_max'],
         'temp_min': weatherData['main']['temp_min'],
-        'weather_description': weatherData['weather'][0]['description'],
+        'weather_description': weatherData['weather'][0]['main'],
+        'weather_description_detail': weatherData['weather'][0]['description'],
         'weather_id': weatherData['weather'][0]['id']}
     }
 
-    api_response = {
+    endpoint_response = {
         'city': weatherData['name'],
          'weather': [dailyWeather]
         }
 
     api_log(request.base_url, zip_code, response.text)
 
-    return jsonify(api_response), 200
+    return jsonify(endpoint_response), 200
 
 @restful_api.route('/weather/forecast')
 def forcastWeather():
@@ -77,19 +78,13 @@ def forcastWeather():
         return jsonify(
             Error='No zip code query provided. Please provide an ID to lookup by - example.com/api/weather/forecast?zip=10018')
 
-    if 'days' in request.args:
-        days = int(request.args['days'])
-    else:
-        days = 5
-
     response = requests.get(
-        'http://api.openweathermap.org/data/2.5/forecast?zip={}&cnt={}&units=imperial&APPID=929be9dbced9ad5fe63ae8c31134eb3f'.format(zip_code,days))
+        'http://api.openweathermap.org/data/2.5/forecast?zip={}&units=imperial&APPID=929be9dbced9ad5fe63ae8c31134eb3f'.format(zip_code))
 
     weatherData = response.json()
 
     print(weatherData)
 
-    day = 0
     dailyWeather = {}
     while day < days:
         print(day)
