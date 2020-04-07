d75f51d326dfacf20217b57c8c1197ce7bf0f28f
Neilson Flemming <NeilsonFlemming@gmail.com>
2020-04-06 21:44:49 -0400

    Updated API json Schemas

--- api/restful_api.py
+++ api/restful_api.py
@@ -1,20 +1,42 @@
 from flask import Blueprint, jsonify, request
-from db.sqlite3 import api_log,api_query
+from db.sqlite3 import api_log, api_query
 import requests
 from datetime import datetime
+import json
 
 restful_api = Blueprint('restful_api', __name__)
+
+
+def weatherEndpoint(zipCode):
+    response = requests.get(
+        'https://api.zip-codes.com/ZipCodesAPI.svc/1.0/QuickGetZipCodeDetails/{}?key=0MB3WYFJGRVXBGDYU3UL'.format(
+            zipCode))
+
+    locationData = response.json()
+    longitude = locationData['Longitude']
+    latitude = locationData['Latitude']
+
+    response = requests.get(
+        'https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&units=imperial&APPID=929be9dbced9ad5fe63ae8c31134eb3f'.format(
+            longitude, latitude))
+
+    weatherData = response.json()
+
+    return(locationData,weatherData)
+
 
 @restful_api.errorhandler(404)
 def page_not_found(e):
     return jsonify(Error='The resource (endpoint) could not be found'), 404
 
+
 @restful_api.route('/')
 def home():
     return jsonify(Message='Welcome to Neilson\'s flask resultful weather API'), 200
 
+
 @restful_api.route('/weather/current')
-def currentWeather():
+def getCurrentWeather():
     # Check if an zip was provided as part of the URL.
     # If zip is provided, assign it to a variable.
     # If no ID is provided, display an error via api.
@@ -24,7 +46,7 @@ def currentWeather():
             if len(request.args['zip']) != 5:
                 raise ValueError('Zip code length is less than or greater than 5')
             else:
-                zip_code = int(request.args['zip'])
+                zipCode = int(request.args['zip'])
         except:
             # Throw error if not valid Zip Code
             return jsonify(Error='Zip provided is not a valid zip code'), 404
@@ -33,35 +55,32 @@ def currentWeather():
         return jsonify(
             Error='No zip code query provided. Please provide an ID to lookup by - example.com/api/weather/current?zip=10018'), 404
 
-    response = requests.get(
-        'http://api.openweathermap.org/data/2.5/weather?zip={}&units=imperial&APPID=929be9dbced9ad5fe63ae8c31134eb3f'.format(zip_code))
+    data = weatherEndpoint(zipCode)
+    locationData = data[0]
+    weatherData = data[1]
 
-    weatherData = response.json()
-
-    now = datetime.now()
-    dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
-
-    dailyWeather = {'data': {
-        'date': dt_string,
-        'temp': weatherData['main']['temp'],
-        'temp_max': weatherData['main']['temp_max'],
-        'temp_min': weatherData['main']['temp_min'],
-        'weather_description': weatherData['weather'][0]['main'],
-        'weather_description_detail': weatherData['weather'][0]['description'],
-        'weather_id': weatherData['weather'][0]['id']}
+    currentWeather = {
+           'date': weatherData['daily'][0]['dt'],
+           'temp': weatherData['daily'][0]['temp']['day'],
+           'temp_max': weatherData['daily'][0]['temp']['max'],
+           'temp_min': weatherData['daily'][0]['temp']['min'],
+           'weather_description': weatherData['daily'][0]['weather'][0]['main'],
+           'weather_description_detail': weatherData['daily'][0]['weather'][0]['description'],
+            'weather_id': weatherData['daily'][0]['weather'][0]['main']
     }
 
     endpoint_response = {
-        'city': weatherData['name'],
-         'weather': [dailyWeather]
-        }
+        'location': locationData,      
+        'weather': currentWeather
+    }
 
-    api_log(request.base_url, zip_code, response.text)
+    api_log(request.base_url, zipCode, json.dumps(endpoint_response))
 
     return jsonify(endpoint_response), 200
 
+
 @restful_api.route('/weather/forecast')
-def forcastWeather():
+def getForcastWeather():
     # Check if an zip was provided as part of the URL.
     # If zip is provided, assign it to a variable.
     # If no ID is provided, display an error via api.
@@ -71,7 +90,7 @@ def forcastWeather():
             if len(request.args['zip']) != 5:
                 raise ValueError('Zip code length is less than or greater than 5')
             else:
-                zip_code = int(request.args['zip'])
+                zipCode = int(request.args['zip'])
         except:
             # Throw error if not valid Zip Code
             return jsonify(Error='Zip provided is not a valid zip code'), 404
@@ -79,36 +98,33 @@ def forcastWeather():
         return jsonify(
             Error='No zip code query provided. Please provide an ID to lookup by - example.com/api/weather/forecast?zip=10018')
 
-    response = requests.get(
-        'http://api.openweathermap.org/data/2.5/forecast?zip={}&units=imperial&APPID=929be9dbced9ad5fe63ae8c31134eb3f'.format(zip_code))
+    data = weatherEndpoint(zipCode)
+    locationData = data[0]
+    weatherData = data[1]
 
-    weatherData = response.json()
+    dailyWeather = []
 
-    print(weatherData)
-
-    dailyWeather = {}
-    for day in len(weatherData):
-        print(day)
-        #for each day in the number of days add data to dictonary
-        data = {weatherData['list'][day]['dt_txt'] : {
-            'temp': weatherData['list'][day]['main']['temp'],
-            'temp_max': weatherData['list'][day]['main']['temp_max'],
-            'temp_min': weatherData['list'][day]['main']['temp_min'],
-            'weather_description': weatherData['list'][day]['weather'][0]['description'],
-            'weather_id': weatherData['list'][day]['weather'][0]['id']}
-             }
-        #append to daily weather to dictionary
-        dailyWeather.update(data)
-        day += 1
-
-    api_response = {
-        'city': weatherData['city']['name'],
-         'weather': [dailyWeather]
+    for day in weatherData['daily']:
+        weather = {
+            'date': day['dt'],
+            'temp': day['temp']['day'],
+            'temp_max': day['temp']['max'],
+            'temp_min': day['temp']['min'],
+            'weather_description': day['weather'][0]['main'],
+            'weather_description_detail': day['weather'][0]['description'],
+            'weather_id': day['weather'][0]['main']
         }
+        dailyWeather.append(weather)
 
-    api_log(request.base_url, zip_code, response.text)
-    # return weatherData, 200
-    return jsonify(api_response), 200
+    endpoint_response = {
+        'location': locationData,
+        'weather': dailyWeather
+    }
+
+    api_log(request.base_url, zipCode, json.dumps(endpoint_response))
+
+    return jsonify(endpoint_response), 200
+
 
 @restful_api.route('/log')
 def request_log():
@@ -125,4 +141,3 @@ def request_log():
     else:
         results = api_query('''select * from request_log''')
         return jsonify(results), 200
-
